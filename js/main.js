f8d9004809c0ed4bbcf8e44a0f797dffee769d98
sslmnsy <160192830+sslmnsy@users.noreply.github.com>
2024-03-08 20:12:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const mntoggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
-
-mntoggle.addEventListener('click',function(){
-   nav.classList.toggle('menushow');
-}) 
+function toggleMenu(){
+   const menu = document.querySelector(".menu-links");
+   const icon = document.querySelector(".hamburger-icon");
+   menu.classList.toggle("open");
+   icon.classList.toggle("open");
+}
